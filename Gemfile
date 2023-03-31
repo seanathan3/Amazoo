c9fc1d9d16de54aa649dd10915ba75993c5eab44
Sean Abbas <sean.abbas33@gmail.com>
2023-03-31 14:34:05 -0400

    basic setup

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ gem "bcrypt"
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "byebug", platforms: %i[ mri mingw x64_mingw ]
+  gem "faker"
 end
 
 group :development do
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as ReactDOMClient from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = ReactDOMClient.createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+)
