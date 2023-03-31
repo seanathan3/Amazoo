c4272198f8fed7670f1cfbd9b07b64f76fd0daf3
Sean Abbas <sean.abbas33@gmail.com>
2023-03-31 15:46:20 -0400

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,42 @@
 #  name            :string           not null
 #
 class User < ApplicationRecord
-    
+    validates :name,
+        presence: true,
+        length: { in: 1..50 },
+        format: { without: URI::MailTo::EMAIL_REGEXP, message: "can't be an email" }
+
+
+    validates :email,
+        presence: true,
+        uniqueness: true,
+        length: { in: 3..255 },
+        format: { with: URI::MailTo::EMAIL_REGEXP }
+
+    validates :password,
+        length: { in: 6..255 },
+        allow_nil: true
+
+    validates :session_token,
+        presence: true,
+        uniqueness: true
+
+    before_validation :ensure_session_token
+
+    private
+    def generate_unique_session_token
+        token = SecureRandom::urlsafe_base64
+        while User.exists?(session_token: token)
+            token = SecureRandom::urlsafe_base64
+        end
+        token
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+
+
+
+
 end
