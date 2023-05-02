02604933cc21e5f4ca050545386bc7617af51eb4
Sean Abbas <sean.abbas33@gmail.com>
2023-05-01 23:57:38 -0400

    star pictures for ratings

--- frontend/src/components/ReviewShow/index.jsx
+++ frontend/src/components/ReviewShow/index.jsx
@@ -4,17 +4,31 @@ import { useEffect } from 'react';
 import userPic from '../../assets/user_pic.jpeg';
 import { fetchUser } from '../../store/userReducer';
 import one from '../../assets/amazon-stars/amazon-stars-2.png'
-import two from '../../assets/amazon-stars/amazon-stars-2.png'
-import three from '../../assets/amazon-stars/amazon-stars-2.png'
-import four from '../../assets/amazon-stars/amazon-stars-2.png'
-import five from '../../assets/amazon-stars/amazon-stars-2.png'
-
+import two from '../../assets/amazon-stars/amazon-stars-4.png'
+import three from '../../assets/amazon-stars/amazon-stars-6.png'
+import four from '../../assets/amazon-stars/amazon-stars-8.png'
+import five from '../../assets/amazon-stars/amazon-stars-10.png'
 
 
 
 const ReviewShow = ({review}) => {
     const dispatch = useDispatch();
     const user = useSelector(state => state.users[review.commenterId])
+    
+    console.log(review.rating)
+
+    let myRating;
+    if (review.rating === "5") {
+        myRating = five;
+    } else if (review.rating === "4") {
+        myRating = four;
+    } else if (review.rating === "3") {
+        myRating = three;
+    } else if (review.rating === "2") {
+        myRating = two;
+    } else {
+        myRating = one;
+    }
 
     useEffect(() => {
             dispatch(fetchUser(review.commenterId))
@@ -28,7 +42,7 @@ const ReviewShow = ({review}) => {
                     <div id="rs-name">{user?.name}</div>
                 </div>
                 <div id="rs-line-2">
-                    <div>{review.rating} stars</div>
+                    <img src={myRating} alt="stars" />
 
                 </div>
                 <div>{review.body}</div>
