bbab9afee36cb75824b2405de1cdcda2fe138986
Sean Abbas <sean.abbas33@gmail.com>
2023-05-08 14:38:21 -0500

    working on display card review aggregation

--- frontend/src/components/ItemIndex/DisplayCard.jsx
+++ frontend/src/components/ItemIndex/DisplayCard.jsx
@@ -1,13 +1,43 @@
 import { Link } from 'react-router-dom';
 import Price from '../Price';
 import './items.css'
-import rating6 from '../../assets/amazon-stars/amazon-stars-6.png';
-import rating7 from '../../assets/amazon-stars/amazon-stars-7.png';
-import rating8 from '../../assets/amazon-stars/amazon-stars-8.png';
-import rating9 from '../../assets/amazon-stars/amazon-stars-9.png';
-import rating10 from '../../assets/amazon-stars/amazon-stars-10.png';
+import one from '../../assets/amazon-stars/amazon-stars-2.png';
+import oneP from '../../assets/amazon-stars/amazon-stars-3.png';
+import two from '../../assets/amazon-stars/amazon-stars-4.png';
+import twoP from '../../assets/amazon-stars/amazon-stars-5.png';
+import three from '../../assets/amazon-stars/amazon-stars-6.png';
+import threeP from '../../assets/amazon-stars/amazon-stars-7.png';
+import four from '../../assets/amazon-stars/amazon-stars-8.png';
+import fourP from '../../assets/amazon-stars/amazon-stars-9.png';
+import five from '../../assets/amazon-stars/amazon-stars-10.png';
+import { calcAverageRating } from '../../utils/utils';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 const DisplayCard = ({item}) => {
+
+    let pic;
+    let myRating = calcAverageRating(item.reviews);
+    console.log(myRating);
+    if (myRating >= 4.7) {
+        pic = five;
+    } else if (myRating >= 4.3) {
+        pic = fourP;
+    } else if (myRating >= 3.7) {
+        pic = four;
+    } else if (myRating >= 3.3) {
+        pic = threeP;
+    } else if (myRating >= 2.7) {
+        pic = three;
+    } else if (myRating >= 2.3) {
+        pic = twoP;
+    } else if (myRating >= 1.7) {
+        pic = two;
+    } else if (myRating >= 1.3) {
+        pic = oneP;
+    } else {
+        pic = one;
+    }
 
     return (
         <>
